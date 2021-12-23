322b4bd078f9305e19f5c6fe9f4b717ab3aeff51
qvolcano <qvolcano@qq.com>
2021-12-23 22:58:58 +0800

    first commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+npm install @egret/setup --global
+egret-setup install engine 5.4.1
+mkdir -p /root/.config/EgretLauncher/download/EgretCompiler
+tar -xf egret-compiler-0.0.4.tar -C /root/.config/EgretLauncher/download/EgretCompiler
